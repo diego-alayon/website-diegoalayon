c5dd3f15f9fcb7ff006c9cd73a3c27a23a868c4b
Diego Alayon Gomez <30353719+diego-alayon@users.noreply.github.com>
2022-04-25 14:17:12 +0200

    Installed fixed fonts woff format type

--- diegoalayon-com/tailwind.config.js
+++ diegoalayon-com/tailwind.config.js
@@ -1,10 +1,29 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sfproDisplayRegular': 'sfpro-display-regular',
+        'sfproDisplayBold': 'sfpro-display-bold'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({addComponents, addBase, addUtilities, theme}){
+      addComponents({
+        'myClass': {}
+      }),
+      addBase({
+        'body': {
+          fontSize: theme('fontFamily.sfproDisplayBold')
+        }
+      })
+    })
+  ],
 }
